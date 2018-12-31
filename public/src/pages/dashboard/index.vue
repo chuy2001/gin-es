0b9fd035fb89ee3da49c5ccd1ae9e4a259c7c2dd
chuy2001 <chuy2001@gmail.com>
2018-12-31 19:24:37 +0800

    initial

--- public/src/pages/dashboard/index.vue
+++ public/src/pages/dashboard/index.vue
@@ -15,12 +15,14 @@
         @moved="movedHandler"
       >
         <el-card shadow="hover" class="page_card">
-          <div slot="header" class="clearfix">
+          <div slot="header" >
             <span>卡片名称{{item.i}}</span>
-            <el-button @click="changeType(index)" style="float: right; padding: 3  px 0" type="text" >切换图形</el-button>
+            <el-button @click="changeType(index)" style="float: right; padding: 0  px 0" type="text" >切换图形</el-button>
           </div>
           <!-- <template v-if="item.i === '0'"> -->
-            <ve-chart :data="chartData" :height="item.chartHight" :settings="item.chartSettings" ref="mychart"></ve-chart>
+          <div>
+            <ve-chart :data="chartData" :width="item.chartWidth" :height="item.chartHight" :settings="item.chartSettings" ref="mychart"></ve-chart>
+          </div>
           <!-- </template> -->
         </el-card>
       </d2-grid-item>
@@ -30,29 +32,17 @@
 
 <script>
 import { Masters } from '@/api'
-const DATA_FROM_BACKEND = {
-  columns: ['date', 'PV', 'Order'],
-  rows: [
-    { 'date': '2018-05-22', 'PV': 32371, 'Order': 19810 },
-    { 'date': '2018-05-23', 'PV': 12328, 'Order': 4398 },
-    { 'date': '2018-05-24', 'PV': 92381, 'Order': 52910 }
-  ]
-}
-const EMPTY_DATA = {
-  columns: [],
-  rows: []
-}
 export default {
   data () {
-    this.typeArr = ['line', 'histogram', 'pie']
+    this.typeArr = ['line', 'histogram', 'pie', 'bar', 'ring', 'waterfall', 'funnel', 'radar', 'map', 'heatmap']
     this.index = 0
     return {
       filename: __filename,
       layout: {
         layout: [
-          { x: 0, y: 0, w: 4, h: 10, i: '0', chartHight: '300px', chartSettings: { type: 'line', metrics: ['value'], dimension: ['key'] } },
-          { x: 4, y: 0, w: 4, h: 10, i: '1', chartHight: '300px', chartSettings: { type: 'histogram' } },
-          { x: 8, y: 0, w: 4, h: 10, i: '2', chartHight: '300px', chartSettings: { type: 'pie' } }
+          { x: 0, y: 0, w: 4, h: 10, i: '0', chartWidth: '400px', chartHight: '300px', chartSettings: { type: 'line', labelMap: { 'v0': 'cpu' } } },
+          { x: 4, y: 0, w: 4, h: 10, i: '1', chartWidth: '400px', chartHight: '300px', chartSettings: { type: 'histogram' } },
+          { x: 8, y: 0, w: 4, h: 10, i: '2', chartWidth: '400px', chartHight: '300px', chartSettings: { type: 'pie' } }
         ],
         colNum: 12,
         rowHeight: 30,
@@ -63,36 +53,31 @@ export default {
         margin: [5, 5],
         useCssTransforms: true
       },
-      // chartData: {
-      //   columns: ['日期', '访问用户', '下单用户', '下单率'],
-      //   rows: [
-      //     { 日期: '1/1', 访问用户: 1393, 下单用户: 1093, 下单率: 0.32 },
-      //     { 日期: '1/2', 访问用户: 3530, 下单用户: 3230, 下单率: 0.26 },
-      //     { 日期: '1/3', 访问用户: 2923, 下单用户: 2623, 下单率: 0.76 },
-      //     { 日期: '1/4', 访问用户: 1723, 下单用户: 1423, 下单率: 0.49 },
-      //     { 日期: '1/5', 访问用户: 3792, 下单用户: 3492, 下单率: 0.323 },
-      //     { 日期: '1/6', 访问用户: 4593, 下单用户: 4293, 下单率: 0.78 }
-      //   ]
-      // },
       chartData: {
-        columns: ['value', 'key_as_string', 'key', 'doc_count'],
+        columns: [],
         rows: []
       },
       loading: false,
       dataEmpty: false,
       chartSettings: { type: this.typeArr[this.index] },
-      formData: ''
+      formData: '',
+      timer: null
     }
   },
   created () {
     // this.formData = '{"index":"telegraf*"}' + '\n' + '{"query":{"match_all":{}}}' + '\n'
-    this.formData = '{"search_type":"query_then_fetch","ignore_unavailable":true,"index":["tele*2018.12.28"]}' + '\n' + '{"size":0,"query":{"bool":{"filter":[{"range":{"@timestamp":{"gte":"1546004110960","lte":"1546004410960","format":"epoch_millis"}}},{"query_string":{"analyze_wildcard":true,"query":"*"}}]}},"aggs":{"2":{"date_histogram":{"interval":"1m","field":"@timestamp","min_doc_count":0,"extended_bounds":{"min":"1546004110960","max":"1546004410960"},"format":"epoch_millis"},"aggs":{"1":{"avg":{"field":"mem.available_percent"}}}}}}' + '\n'
-    // this.getData()
+    this.formData = '{"search_type":"query_then_fetch","ignore_unavailable":true,"index":["tele*"]}' + '\n' + '{"size":0,"query":{"bool":{"filter":[{"range":{"@timestamp":{"gte":"1546004110960","lte":"1546004410960","format":"epoch_millis"}}},{"query_string":{"analyze_wildcard":true,"query":"*"}}]}},"aggs":{"2":{"date_histogram":{"interval":"30s","field":"@timestamp","min_doc_count":0,"extended_bounds":{"min":"1546004110960","max":"1546004410960"},"format":"epoch_millis"},"aggs":{"1":{"avg":{"field":"mem.available_percent"}}}}}}' + '\n'
+    this.getData()
   },
   mounted () {
     // 加载完成后显示提示
     this.showInfo()
     window.vue = this
+  },
+  beforeDestroy () {
+    if (this.timer) { // 如果定时器还在运行 或者直接关闭，不用判断
+      clearInterval(this.timer) //  关闭
+    }
   },
   methods: {
     log (arg1 = 'log', ...logs) {
@@ -136,6 +121,7 @@ export default {
         `i: ${i}, newH: ${newH}, newW: ${newW}, newHPx: ${newHPx}, newWPx: ${newWPx}`
       )
       this.layout.layout[i].chartHight = (newHPx - 40) + 'px'
+      this.layout.layout[i].chartWidth = '100%'
       this.$refs.mychart[i].echarts.resize()
     },
     movedHandler (i, newX, newY) {
@@ -145,25 +131,47 @@ export default {
       this.index++
       if (this.index >= this.typeArr.length) { this.index = 0 }
       this.layout.layout[index].chartSettings.type = this.typeArr[this.index]
-      this.getESData()
+      // this.getData()
     },
     getData () {
       this.loading = true
       // ajax get data ....
-      setInterval(() => {
-        // this.chartData = this.chartData.rows.length ? EMPTY_DATA : DATA_FROM_BACKEND
-        this.chartData = DATA_FROM_BACKEND
-        this.dataEmpty = !this.chartData.rows.length
-        this.loading = false
-      }, 3000)
+      this.getESData()
+      this.timer = setInterval(() => {
+        this.getESData()
+      }, 10000)
     },
     getESData () {
-      Masters.post(`es/msearch`, this.formData)
+      var d = new Date()
+      var currentT = d.getTime()
+      var req = this.formData
+      var minT = /1546004110960/g
+      req = req.replace(minT, currentT - 300000)
+      var maxT = /1546004410960/g
+      req = req.replace(maxT, currentT)
+      Masters.post(`es/msearch`, req)
         .then(response => {
-          // this.tables = response[`data`]
-          //  this.tables = response.data.cmlist
-          this.chartData.rows = response.data.Message[0].aggregations[2].buckets
-          console.log('msg:' + this.chartData)
+          var lengthRow = response.data.Message.length
+          var lengthRows = response.data.Message[0].aggregations[2].buckets.length
+          var tmpRows = []
+          var tmpCol = ['date']
+          d.getTime()
+          if (lengthRow > 0 && lengthRows > 0) {
+            for (var i = 0; i < lengthRows; i++) {
+              var tmpRow = Object()
+              tmpRow['date'] = new Date(response.data.Message[0].aggregations[2].buckets[i].key).toLocaleTimeString()
+              for (var j = 0; j < lengthRow; j++) {
+                tmpRow['v' + j] = response.data.Message[j].aggregations[2].buckets[i]['1'].value
+              }
+              tmpRows.push(tmpRow)
+            }
+            for (var k = 0; k < lengthRow; k++) {
+              tmpCol.push('v' + k)
+            }
+          }
+          this.chartData.rows = tmpRows
+          this.chartData.columns = tmpCol
+          // console.log('msg:' + this.chartData.rows)
         })
         .catch(error => {
           console.log('err:' + error)
